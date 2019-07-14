e57eb620614368103b509e4ac4926de07a77f44d
Cata001 <catalinateguiani@yahoo.com>
2019-07-14 23:47:16 +0100

    Filter Products Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Products from './components/Products';
+import Filter from './components/Filter';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = { size: '', sort: '', cartItems: [], products: [], filteredProducts: [] };
+    this.handleChangeSort = this.handleChangeSort.bind(this);
+    this.handleChangeSize = this.handleChangeSize.bind(this);
   }
   componentWillMount() {
     fetch('http://localhost:8000/products').then(res => res.json())
@@ -15,7 +18,36 @@ class App extends Component {
         
       });
   }
+  handleChangeSort(e) {
+    this.setState({sort: e.target.value});
+    this.listProducts();
+  }
+  handleChangeSize(e) {
+    this.setState({size: e.target.value});
+    this.listProducts();
+  }
+  // handleChangeSort(e) {
+  //   this.setState({sort: e.target.value});
+  //   this.listProducts();
+  // }
 
+  listProducts = () => {
+    this.setState(state => {
+      if (state.sort !== '') {
+        state.products.sort((a, b) =>
+          (state.sort === 'lowest') ?
+             ((a.price > b.price)?1:-1)
+            : ((a.price < b.price) ? 1 : -1));
+      } else {
+        state.products.sort((a, b) => (a.id > b.id) ? 1 : -1);
+      }
+      if (state.size !== '') {
+        return { filteredProducts: state.products.filter(a => a.availableSizes.indexOf(state.size.toUpperCase()) >= 0) };
+      }
+      return { filteredProducts: state.products };
+    })
+  }
+ 
   render() {
     return (
       <div className="container">
@@ -23,7 +55,8 @@ class App extends Component {
         <hr />
         <div className="row">
           <div className="col-md-8">
-            
+            <Filter size={this.state.size} sort={this.state.sort} handleChangeSize={this.handleChangeSize}
+            handleChangeSort={this.handleChangeSort} count={this.state.filteredProducts.length} />            
             <hr />
             <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart} />
           </div>
